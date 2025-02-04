f0229ff67c9ab8f76a47d4756cf5fcfdf275d400
Tony Rodrigues <88041342+TonyRodIv@users.noreply.github.com>
2025-02-04 19:44:56 -0300

    adiciona workflow de deploy para GitHub Pages e ajusta estilo do App

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ import AddTasks from './components/AddTasks'
 
 function App() {
   return(
-    <div>
-      <h1 className='text-red-600'>Gerenciador de tarefas</h1>
+    <div className='w-screen h-screen bg-slate-500 flex justify-center p-6'>
+      <h1 className='text-red-600 text-3xl'>Gerenciador de tarefas</h1>
       <AddTasks />
       <Tasks />
     </div>
